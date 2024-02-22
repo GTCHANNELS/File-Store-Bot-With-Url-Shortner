400a6373de3552af1e8978f683f884312b0eb1df
GTCHANNELS <108328260+GTCHANNELS@users.noreply.github.com>
2024-02-22 18:27:44 +0530

    Update force_sub_handler.py

--- handlers/force_sub_handler.py
+++ handlers/force_sub_handler.py
@@ -50,7 +50,7 @@ async def handle_force_sub(bot: Client, cmd: Message):
                         InlineKeyboardButton("Join UniHub Official Channel", url=invite_link.invite_link)
                     ],
                     [
-                        InlineKeyboardButton("🔄 Refresh 🔄", callback_data="refresh")
+                        InlineKeyboardButton("Try Again", url="https://t.me/{client.username}?start={message.command[1]}")
                     ]
                 ]
             )
